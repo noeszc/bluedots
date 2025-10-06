4b539e5ed1a0ba8e410bd26d7ca2a4c9a5b2e99e
Noé Sánchez <noeszc@gmail.com>
2025-10-06 14:06:06 -0600

    feat: commercial guis

--- install/install-deps.sh
+++ install/install-deps.sh
@@ -25,6 +25,20 @@ sudo dnf install -y \
     mise \
     ghostty
 
+
+# Commercial GUIs
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub \
+    com.google.Chrome \
+    com.slack.Slack \
+    com.spotify.Client
+
+if ! command -v 1password > /dev/null 2>&1; then
+    sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
+    sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+    sudo dnf install -y 1password
+fi
+
 if [ "$SHELL" != "$(which zsh)" ]; then
     chsh -s $(which zsh)
 fi
