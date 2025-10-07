11a287b61ab99710db6105e72b83aaf91aebe779
Noé Sánchez <noeszc@gmail.com>
2025-10-06 23:55:47 -0600

    modus themes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,17 @@
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out =
-        vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out =
+    vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,25 +21,38 @@ require "user.options"
 require "user.keymaps"
 
 require("lazy").setup {
-    spec = {
-        { "folke/ts-comments.nvim", opts = {}, event = "BufReadPre" },
-        {
-            "folke/todo-comments.nvim",
-            dependencies = { "nvim-lua/plenary.nvim" },
-            opts = {},
-            event = "BufReadPre",
-        },
-        {
-            "christoomey/vim-tmux-navigator",
-            keys = {
-                { "<C-H>", "<cmd><C-U>TmuxNavigateLeft<cr>",  silent = true },
-                { "<C-J>", "<cmd><C-U>TmuxNavigateDown<cr>",  silent = true },
-                { "<C-K>", "<cmd><C-U>TmuxNavigateUp<cr>",    silent = true },
-                { "<C-L>", "<cmd><C-U>TmuxNavigateRight<cr>", silent = true },
-            },
-        },
-        { import = "plugins" },
+  spec = {
+    {
+      "miikanissi/modus-themes.nvim",
+      priority = 1000,
+      config = function()
+        require("modus-themes").setup {
+          style = "auto",
+          variant = "tritanopia",
+          comments = { italic = false },
+          keywords = { italic = false },
+        }
+        vim.cmd "colorscheme modus"
+      end,
     },
-    defaults = { lazy = false },
-    install = { colorscheme = { "tokyonight-moon" } },
+    { "folke/ts-comments.nvim", opts = {}, event = "BufReadPre" },
+    {
+      "folke/todo-comments.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      opts = {},
+      event = "BufReadPre",
+    },
+    {
+      "christoomey/vim-tmux-navigator",
+      keys = {
+        { "<C-H>", "<cmd><C-U>TmuxNavigateLeft<cr>", silent = true },
+        { "<C-J>", "<cmd><C-U>TmuxNavigateDown<cr>", silent = true },
+        { "<C-K>", "<cmd><C-U>TmuxNavigateUp<cr>", silent = true },
+        { "<C-L>", "<cmd><C-U>TmuxNavigateRight<cr>", silent = true },
+      },
+    },
+    { import = "plugins" },
+  },
+  defaults = { lazy = false },
+  install = { colorscheme = { "tokyonight-moon" } },
 }
